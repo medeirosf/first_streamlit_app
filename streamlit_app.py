fd61549f2a85107a099c5cf3cc549081f5f8b9e8
Fernando Medeiros <36306858+medeirosf@users.noreply.github.com>
2022-06-02 13:38:06 -0400

    Update streamlit_app.py
    added normalized json response

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,15 +14,20 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index( 'Fruit')
 
-#let's put a pick list here so they can pick the fruit they want to include
+# let's put a pick list here so they can pick the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
                                    
-#display the table on the page
+# display the table on the page
 streamlit.dataframe(fruits_to_show)
 
-#new section to display fruityvice api response
+# new section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) # just writes the data to the screen
+
+# take the json version of the response and normalize it
+fruityvice_normalized = pandas.json(normalize(fruityvice_response.json())
+#output it the screen as a table
+streamlit.dataframe(fruityvice_normalized)
