fe86253c58bb814d0b9ddd23717d69c2f637ab71
Fernando Medeiros <36306858+medeirosf@users.noreply.github.com>
2022-06-03 15:27:43 -0400

    Update streamlit_app.py
    added close connection code
    moved STOP command and commented it out

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,9 +55,6 @@ if streamlit.button('View Our Fruit List - Add Your Favorites!'):
   my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
-# don't run anything past here while we troubleshoot
-streamlit.stop()
-
 # allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
@@ -68,6 +65,8 @@ add_my_fruit = streamlit.text_input('What fruit would you like information about
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
   streamlit.text(back_from_function)
 
-
+# don't run anything past here while we troubleshoot
+#streamlit.stop()
